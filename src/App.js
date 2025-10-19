b3579f5886b89f1e08cf2ea7fd20ba2659d44d4d
jiwonee02 <nepthun@naver.com>
2025-10-19 21:21:03 +0900

    feat: 양의 정수만 허용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
 
 const DEFAULT_DELIMS_REGEX = /[,:]/;
+const POSITIVE_INTREGEX = /^[1-9]\d*$/;
+
+const ERROR = {
+    NOT_POSITIVE_INT: "[ERROR] 양의 정수만 입력할 수 있습니다.",
+};
 
 class App {
     async run() {
@@ -15,6 +20,9 @@ export default App;
 
 export function calculate(input) {
     if (input === "") return 0;
-    const parts = input.split(DEFAULT_DELIMS_REGEX);
-    return parts.map(Number).reduce((a, b) => a + b, 0);
+    const tokens = input.split(DEFAULT_DELIMS_REGEX);
+    tokens.forEach((t) => {
+        if (!POSITIVE_INTREGEX.test(t)) throw new Error(ERROR.NOT_POSITIVE_INT);
+    });
+    return tokens.reduce((acc, t) => acc + Number(t), 0);
 }
